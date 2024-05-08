c5836b453b3d0a20b0fe1819dd612235a3645bda
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2024-05-08 13:35:26 +0530

    commit dashboard with sidebar

--- parking-management/app/Http/Controllers/SSadminController.php
+++ parking-management/app/Http/Controllers/SSadminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class SsadminController extends Controller
 {
     public function SsadminDashboard(){
-        return view("ssadmin.ssadmin_dashboard");
+        return view("ssadmin.index");
     } //end method
 }
